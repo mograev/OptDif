22ccfe8c83668345d3b8552e740e5b8a18887bd9
Moritz Grävinghoff <mograev@gmail.com>
2025-06-13 00:12:35 +0200

    updated paths and gitignore

--- src/__init__.py
+++ src/__init__.py
@@ -3,8 +3,10 @@ from pathlib import Path
 # Try to locate the DNGO training script
 try:
     DNGO_TRAIN_FILE = str(Path(__path__[0]) / "bo/dngo_train.py")
+    DNGO_PCA_TRAIN_FILE = str(Path(__path__[0]) / "bo/dngo_train_pca.py")
     GP_TRAIN_FILE = str(Path(__path__[0]) / "bo/gp_train.py")
     BO_OPT_FILE = str(Path(__path__[0]) / "bo/opt.py")
+    BO_PCA_OPT_FILE = str(Path(__path__[0]) / "bo/opt_pca.py")
     GBO_TRAIN_FILE = str(Path(__path__[0]) / "gbo/gbo_train.py")
     GBO_OPT_FILE = str(Path(__path__[0]) / "gbo/gbo_opt.py")
     GBO_PCA_TRAIN_FILE = str(Path(__path__[0]) / "gbo/gbo_train_pca.py")
