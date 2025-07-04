07060e8103c8ec3a6bf03c51124b4987913e8134
Moritz Grävinghoff <mograev@gmail.com>
2025-07-04 21:49:14 +0200

    updated smile classification

--- src/run/initial_smile_classification.py
+++ src/run/initial_smile_classification.py
@@ -1,32 +1,27 @@
 import os
 import json
-from tqdm import tqdm
 import torch
 from src.classification.smile_classifier import SmileClassifier
 
 # Configuration
-PRETRAINED_CLASSIFIER_PATH = "models/classifier/celeba_smile/predictor_128.pth.tar"
-SCALED_CLASSIFIER_PATH = "models/classifier/celeba_smile/predictor_128_scaled3.pth.tar"
+CLASSIFIER_PATH = "models/classifier/celeba_smile/predictor_128.pth.tar" # or scaled: "models/classifier/celeba_smile/predictor_128_scaled3.pth.tar"
 ATTR_FILE = "models/classifier/celeba_smile/attributes.json"
 IMAGE_PATH = "data/ffhq/images1024x1024/"
 OUTPUT_FILE = "data/ffhq/ffhq_smile_scores_scaled.json"
 
 # Create classifier
 device = "cuda" if torch.cuda.is_available() else "cpu"
-smile_classifier = SmileClassifier(PRETRAINED_CLASSIFIER_PATH, ATTR_FILE, SCALED_CLASSIFIER_PATH, device=device)
+smile_classifier = SmileClassifier(CLASSIFIER_PATH, ATTR_FILE, scaled=False, device=device) # Change to True for scaled classifier
 
 # Get all images in the dataset directory
-print("Classifying images...")
 filenames = os.listdir(IMAGE_PATH)
-# Ensure it's an image file
-image_filenames = [filename for filename in filenames if filename.lower().endswith(('.png', '.jpg', '.jpeg'))]
-# Sort the filenames so that the order is consistent
+image_filenames = [filename for filename in filenames if filename.lower().endswith(('.png'))]
 image_filenames.sort()
-# Get the full filepaths
 image_filepaths = [os.path.join(IMAGE_PATH, filename) for filename in image_filenames]
 
 # Classify the images
-smile_scores = smile_classifier.classify(image_filepaths)
+print(f"Classifying {len(image_filepaths)} images...")
+smile_scores = smile_classifier.classify_from_path(image_filepaths)
 
 # Create a dictionary of image filenames and their corresponding smile scores
 smile_scores = {filename: smile_score for filename, smile_score in zip(image_filenames, smile_scores)}
